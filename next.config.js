8340a22382a47017110437eaf31a13d2ccf2417c
Norbert de Langen <ndelangen@me.com>
2017-12-28 23:45:17 +0100

    ADD config for static export

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  exportPathMap() {
+    return {
+      '/': { page: '/' },
+      '/preview-1': { page: '/preview-1' },
+      '/preview-2': { page: '/preview-2' },
+      '/preview-3': { page: '/preview-3' },
+    }
+  },
+};
