85782bde3dcda952cd178b182e6d12c7a8628ac4
Norbert de Langen <ndelangen@me.com>
2018-01-11 23:29:43 +0100

    IMPROVE the main api

--- core/main.js
+++ core/main.js
@@ -1,12 +1,50 @@
 const collection = {};
+const subscribers = [];
+
+const broadcast = (...params) => subscribers.forEach(fn => fn(...params));
+const subscribe = fn => subscribers.push(fn);
+const read = () => collection;
+
+const describe = (name1, options1 = {}) => {
+  const decorators = [];
+  const options = [];
+  const api = {
+    decorate: fn => {
+      decorators.push(fn);
+      return api;
+    },
+    options: obj => {
+      options.push(obj);
+      return api;
+    },
+    add: (name2, render, options2 = {}) => {
+      const before = collection[name1];
+      const after = Object.assign(collection[name1] || {}, {
+        [name2]: {
+          render,
+          options: Object.assign({}, options1, ...options, options2, {
+            decorators: []
+              .concat(options1.decorators)
+              .concat(options.decorators)
+              .concat(decorators)
+              .concat(options2.decorators),
+          }),
+        },
+      });
+
+      broadcast({ before, after });
+
+      collection[name1] = after;
+
+      return api;
+    },
+    read,
+  };
+  return api;
+};
 
 module.exports = {
-  describe: (name1, options1) => ({
-    add: (name2, render, options2 = {}) => {
-      collection[name1] = Object.assign(collection[name1] || {}, {
-        [name2]: { render, options: Object.assign({}, options1, options2) },
-      });
-    },
-    read: () => collection,
-  }),
+  broadcast,
+  subscribe,
+  describe,
 };
